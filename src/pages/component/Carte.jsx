ac936cf86bca998bf4ade6afec0b0ba016f77425
oussi <oussi@MacBook-Pro-de-oussi.local>
2025-04-04 12:06:59 +0200

    first

---
+++ src/pages/component/Carte.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export function Carte() {
+    return (
+        <div style={{ height: '550px', width: '900px' }}>
+            
+            <MapContainer
+                center={[46.603354, 1.888334]} 
+                zoom={6} 
+                style={{ height: '100%', width: '100%' }} 
+            >
+                
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                
+                <Marker position={[48.8566, 2.3522]}> 
+                    <Popup>
+                        Paris, France
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+
+import { useState, useEffect } from "react";
+import { Carte } from './component/Carte';  
+
+
+export default function Home() {
+    const [annee, setAnnee] = useState([]);
+    useEffect(() => {
+        const dateFin = new Date().getFullYear();
+        const dateDebut = 1900;
+        const annees= [];
+        for (let i = dateFin; i >= dateDebut; i--) {
+            annees.push(i);
+        }
+        setAnnee(annees);
+    }, []);
+
+    return (
+        <>
+        <div className="bg-gray-950 min-h-screen flex flex-col items-center justify-center space-y-4 p-4 ">
+            <div className="text-white text-2xl font-bold space-x-4">
+
+                <input
+                    type="text"
+                    placeholder="Recherche..."
+                    className="p-2 rounded border border-gray-950"/>
+
+                <select className="p-2 rounded border border-gray-950">
+                    {
+                       annee.map((annee) => (
+                            <option key={annee} value={annee}>{annee}</option>
+                        )
+                       )
+                    }
+                </select>
+            </div>
+            
+            <Carte />
+        </div>
+        </>
+    );
+}
